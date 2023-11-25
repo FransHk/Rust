898280139fd6e134b0a2a3153f3d2488b3191fca
Frans <frans-huntink@live.nl>
2023-11-25 21:32:28 +0100

    Rust update

--- thijs_game/src/main.rs
+++ thijs_game/src/main.rs
@@ -1,13 +1,13 @@
 use rand::Rng;
 
 fn main() {
-    let game_set = init(2, 14, 3, 2);
-    let (players, game) = instantiate_players(game_set, 2);
-    debug_game(&game, &players);
+    let game_set = init(2, 14, 3, 2); // Init initial game settings 
+    let game_set = instantiate_players(game_set, 2); // Update by adding players to game
+    debug_game(&game_set); // Debug current game properties
 }
 
 // Print game, console friendly
-fn debug_game(game_set: &Game, players: &Vec<Player>) {
+fn debug_game(game_set: &Game) {
    println!("---------------------------------");    
    println!("Debugging game with {} players and {} max turns", &game_set.player_num, &game_set.max_turns);
    println!("Cards still in the current game pool (not drawn): {:?}", game_set.pool);
@@ -22,14 +22,15 @@ fn debug_game(game_set: &Game, players: &Vec<Player>) {
         _ =>  println!("{}", element.to_string()), 
    }};
 
-   for element in players {
+   for element in &game_set.players {
     println!("Player: {} has card {}", element.id, element.card);
    }
 
 }
 
-
-fn instantiate_players(mut game_set: Game, amount: i8) -> (Vec<Player>, Game){
+// Accepts Game struct, updates its card pool and
+// the active players in the game.
+fn instantiate_players(mut game_set: Game, amount: i8) -> Game {
     let mut players = Vec::new();
     let mut bot: bool = false; 
 
@@ -44,9 +45,9 @@ fn instantiate_players(mut game_set: Game, amount: i8) -> (Vec<Player>, Game){
 
         bot = true; // ensure all but first player are set to be bots
     }
-
+    game_set.players = players;
     //println!("Pool after drawing cards: {:?}", game_set.pool);
-    (players, game_set)
+    game_set
 }
 
 fn game_loop(game_set: &Game) {
@@ -63,7 +64,7 @@ struct Game {
     pool: Vec<i8>,
     player_num: i8,
     max_turns: i8,
-    //players: Vec<Player>,
+    players: Vec<Player>,
 }
 
 // Return initialised game settings object
@@ -81,7 +82,8 @@ fn init(mut min: i8, mut max: i8, player_num: i8, max_turns: i8) -> Game {
     let game_set = Game {
         pool: nums,
         player_num: player_num,
-        max_turns: max_turns
+        max_turns: max_turns,
+        players: Vec::<Player>::new(), // TODO figure out if this is OK?
     };
     return game_set   
 }
